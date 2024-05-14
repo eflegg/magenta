ab6dca7e558ebb7d3f5c33eb018290303d93993e
Erin Flegg <ehflegg@gmail.com>
2024-05-14 11:57:47 -0700

    category post listing page

--- category.php
+++ category.php
@@ -8,15 +8,18 @@ Template Name: Category Blog Page
 get_header(); ?>
 
 <?php if ( have_posts() ) : ?>
-    <p>Category: <?php single_cat_title(); ?></p>
+    <h1 class="text-center"><?php single_cat_title(); ?></h1>
     <?php
     // Show an optional term description.
     $term_description = term_description();
     if ( ! empty( $term_description ) ) :
-        printf( '<div class="taxonomy-description">%s</div>', $term_description );
+        printf( '<div class="text-center taxonomy-description">%s</div>', $term_description );
     endif;
 ?>
+
+<ul class="card-container section-container">
 	<?php while ( have_posts() ) : the_post(); ?>
+
     <?php include 'components/cards/blog-card.php'; ?>
 
 
@@ -24,6 +27,8 @@ get_header(); ?>
     <?php endwhile; ?>
 
 
+</ul>
     <?php endif; ?>
+
 
 <?php get_footer(); ?>
